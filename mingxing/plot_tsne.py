09996734fa7258f7b6afa63f58c84ec678f181e9
Ethan <mx.rao.ethan@gmail.com>
2022-09-21 16:45:06 +0800

    change plot_tsne

--- mingxing/plot_tsne.py
+++ mingxing/plot_tsne.py
@@ -3,6 +3,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
+def process_latent(gt_path, contignamepath, cutoff = 1000):
+    ground_truth = dict()
+    gt_bins = set()
+    with open(gt_path, 'r') as f:
+        for l in f.readlines():
+            items = l.split(',')
+            if len(items) == 3:
+                continue
+            temp = items[0].split('_')
+            if int(temp[3]) >= cutoff:
+                ground_truth[items[0]] = items[1]
+                # ground_truth[temp[1]] = items[1]
+                gt_bins.add(items[1])
+
+    temp_list = list(gt_bins)
+    temp_list.sort()
+    label_dict = dict(zip(temp_list, range(len(gt_bins))))
+    contignames = np.load(contignamepath)
+    contignames = contignames['arr_0']
+
+    labels = []
+    for contig in contignames:
+        try:
+            # if label_dict[str(ground_truth[str(int(contig))])] not in [10,11, 12,15]:
+            #     labels.append(-1)
+            # else:
+            #     if label_dict[str(ground_truth[str(int(contig))])] == 10:
+            #         labels.append(25)
+            #     elif label_dict[str(ground_truth[str(int(contig))])] == 11:
+            #         labels.append(20)
+            #     elif label_dict[str(ground_truth[str(int(contig))])] == 12:
+            #         labels.append(15)
+            #     elif label_dict[str(ground_truth[str(int(contig))])] == 15:
+            #         labels.append(10)
+            # labels.append(label_dict[str(ground_truth[str(int(contig))])])
+            labels.append(label_dict[str(ground_truth[str(contig)])])
+        except KeyError:
+            labels.append(-1)
+
+    return np.array(labels)
 
 def draw_figure(latent, labels):
     pass
@@ -19,13 +59,18 @@ def load_csv(csv_path):
 
 
 if __name__ == "__main__":
-    latent_path = "/home/eddie/mingxing_temp/latent600.npy"
-    labels_path = "/home/eddie/mingxing_temp/gmm_epoch_600.csv"
+    latent_path = "/tmp/local/zmzhang/DeepMetaBin/CAMI1/low/vamb_1000/vamb_out/latent.npz"
+    labels_path = "/tmp/local/zmzhang/DeepMetaBin/CAMI1/low/deepmetabin/gmvae_3000/latent50_result.csv"    
+    output_path = "/tmp/local/zmzhang/DeepMetaBin/mingxing/work_with_wc/Metagenomic-Binning/graphs/vamb_with_vamb_nc.png"
 
-    output_path = "/home/eddie/mingxing_temp/gmm_600.png"
+    gt_path = "/tmp/local/zmzhang/DeepMetaBin/CAMI1/low/vamb_1000/vamb_out/nc_results.tsv"
+    contignamepath = "/tmp/local/zmzhang/DeepMetaBin/CAMI1/low/vamb_1000/vamb_out/contignames.npz"
 
     latent = np.load(latent_path) # 8045, 32
-    labels = load_csv(labels_path)
+    latent = latent['arr_0']
+    # labels = load_csv(labels_path)
+    labels = process_latent(gt_path, contignamepath)
+
     tsne = TSNE(
         n_components=2,
         learning_rate="auto",
@@ -39,5 +84,6 @@ if __name__ == "__main__":
         marker=".",
         s=10,
     )
+    plt.colorbar()
     figure.savefig(output_path)
 
